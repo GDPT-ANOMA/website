71cd9abbc239dd8baa6cd6680f1cc29d9c21565b
chark1es <charles15851@gmail.com>
2024-03-23 02:49:05 -0700

    added gdpt colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gdpt: "#007c3d",
+                gdptHover: "#006030",
+                gdptBackground: "#09723D",
+            },
+        },
     },
     plugins: [],
 };
