3af66bca5e5dceda119cb9d8477d0f96707e17dc
chark1es <charles15851@gmail.com>
2024-12-11 04:57:42 -0800

    added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [icon(), tailwind(), mdx(), react()],
+    output: "server",
+    adapter: node({
+        mode: "standalone",
+    }),
 });
